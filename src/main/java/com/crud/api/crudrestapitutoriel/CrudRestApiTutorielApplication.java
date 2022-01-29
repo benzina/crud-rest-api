0997555d2351dbba29d71d1ecf23584ff4c62d52
EL MAHDI <elmahdi.benzina@enosys.org>
2022-01-29 20:14:11 +0100

    initial commit

---
+++ src/main/java/com/crud/api/crudrestapitutoriel/CrudRestApiTutorielApplication.java
@@ -0,0 +1,13 @@
+package com.crud.api.crudrestapitutoriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestApiTutorielApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRestApiTutorielApplication.class, args);
+    }
+
+}
